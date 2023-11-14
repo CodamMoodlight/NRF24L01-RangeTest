cc339073fedd525b1dc42a77dc41c2bb66a4519f
joppe <jboeve009@gmail.com>
2023-11-14 23:23:18 +0100

    Add LowPower lib to save some power. Got a basic thing working where the transimitter is waking up every second to send a radio thing.

--- src/remote/main.cpp
+++ src/remote/main.cpp
@@ -1,28 +1,54 @@
 #include <SPI.h>
-#include "printf.h"
 #include "RF24.h"
 #include "config.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <LowPower.h>
 
 #define CE_PIN 7
 #define CSN_PIN 8
 
-#define BTN_PIN 5
+#define BTN_PIN 0
+#define LED_PIN 9
 
+// #define PRINT
 
 RF24 radio(CE_PIN, CSN_PIN);
 float payload = 0.0;
+unsigned long time_old;
 
-void setup()
+
+void radioSend()
 {
-    pinMode(BTN_PIN, INPUT_PULLUP);
-    Serial.begin(115200);
+    // This device is a TX node
+    bool report = radio.write(&payload, sizeof(float)); // transmit & save the report
 
+    if (report)
+    {
+#ifdef PRINT
+        Serial.print(F("Transmission successful! ")); // payload was delivered
+        Serial.print(F("Time to transmit = "));
+        Serial.print(F(" us. Sent: "));
+        Serial.println(payload); // print payload sent
+#endif
+        payload += 0.01; // increment float payload
+    }
+    else
+    {
+#ifdef PRINT
+        Serial.println(F("Transmission failed or timed out")); // payload was not delivered
+#endif
+    }
+}
+
+void setup_radio()
+{
     // initialize the transceiver on the SPI bus
     if (!radio.begin())
     {
+#ifdef PRINT
         Serial.println(F("radio hardware is not responding!!"));
+#endif
         while (1)
             ;
     }
@@ -34,32 +60,34 @@ void setup()
     radio.openWritingPipe(RADIO_ADDR[ADDR_LED]); // always uses pipe 0
 }
 
-void radioSend()
+void interrupt_cb()
 {
-    // This device is a TX node
-    unsigned long start_timer = micros();               // start the timer
-    bool report = radio.write(&payload, sizeof(float)); // transmit & save the report
-    unsigned long end_timer = micros();                 // end the timer
-
-    if (report)
-    {
-        Serial.print(F("Transmission successful! ")); // payload was delivered
-        Serial.print(F("Time to transmit = "));
-        Serial.print(end_timer - start_timer); // print the timer result
-        Serial.print(F(" us. Sent: "));
-        Serial.println(payload); // print payload sent
-        payload += 0.01;         // increment float payload
-    }
-    else
-    {
-        Serial.println(F("Transmission failed or timed out")); // payload was not delivered
-    }
-}
-void loop()
-{
-    if (!digitalRead(BTN_PIN))
+    if (millis() - time_old > 200)
     {
         radioSend();
-        delay(200);
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        time_old = millis();
     }
+}
+
+void setup()
+{
+    delay(5000);
+    time_old = millis();
+
+    // pinMode(BTN_PIN, INPUT_PULLUP);
+    // 32U4 Interrupt pins 0, 1, 2, 3, 7
+    // attachInterrupt(digitalPinToInterrupt(BTN_PIN), interrupt_cb, LOW);
+
+    setup_radio();
+#ifdef PRINT
+    Serial.begin(115200);
+#endif
+
+}
+
+void loop()
+{
+    LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+    radioSend();
 }
